6bd8855c906be5de8b219e47cdda37bb41c052d7
JBorad988 <jayborad366@gmail.com>
2024-05-17 10:52:15 +0530

    17-MAY-10:55

--- src/app/forms/permisiion-tree-client/permisiion-tree-client.component.ts
+++ src/app/forms/permisiion-tree-client/permisiion-tree-client.component.ts
@@ -57,54 +57,54 @@ export class PermisiionTreeClientComponent implements OnInit {
 
   }
 
-  checkboxChange(args: any): void {
-    debugger
-    const isChecked = args.checked;
-    const featureId = args.rowData.FeatureId;
-    const parentId = args.rowData.ParentId
+  // checkboxChange(args: any): void {
+  //   debugger
+  //   const isChecked = args.checked;
+  //   const featureId = args.rowData.FeatureId;
+  //   const parentId = args.rowData.ParentId
+  //
+  //   console.log(args);
+  //   this.data = this.data || [];
+  //   let updatedData = this.updateFeaturePermissions(this.data, featureId, isChecked, parentId);
+  //   if (updatedData) {
+  //     this.data = updatedData;
+  //   }
+  // }
 
-    console.log(args);
-    this.data = this.data || [];
-    let updatedData = this.updateFeaturePermissions(this.data, featureId, isChecked, parentId);
-    if (updatedData) {
-      this.data = updatedData;
-    }
-  }
+  // updateFeaturePermissions(data: any[], featureId: number, isChecked: boolean, parentId: number): any[] | null {
+  //   return data.map((item: any) => {
+  //     if (item.FeatureId === featureId) {
+  //       item.View = isChecked;
+  //       item.Add = isChecked;
+  //       item.Edit = isChecked;
+  //       item.Active = isChecked;
+  //       item.Delete = isChecked;
+  //       item.Export = isChecked;
+  //       if (item.subtasks) {
+  //         debugger
+  //         item.subtasks = this.updateSubtaskPermissions(item.subtasks, featureId, parentId, isChecked);
+  //       }
+  //     }
+  //     else if (item.subtasks) {
+  //       item.subtasks = this.updateSubtaskPermissions(item.subtasks, featureId, parentId, isChecked);
+  //     }
+  //     return item;
+  //   });
+  // }
 
-  updateFeaturePermissions(data: any[], featureId: number, isChecked: boolean, parentId: number): any[] | null {
-    return data.map((item: any) => {
-      if (item.FeatureId === featureId) {
-        item.View = isChecked;
-        item.Add = isChecked;
-        item.Edit = isChecked;
-        item.Active = isChecked;
-        item.Delete = isChecked;
-        item.Export = isChecked;
-        if (item.subtasks) {
-          debugger
-          item.subtasks = this.updateSubtaskPermissions(item.subtasks, featureId, parentId, isChecked);
-        }
-      }
-      else if (item.subtasks) {
-        item.subtasks = this.updateSubtaskPermissions(item.subtasks, featureId, parentId, isChecked);
-      }
-      return item;
-    });
-  }
-
-  updateSubtaskPermissions(subtasks: any[], featureId: number, parentId: number, isChecked: boolean): any[] {
-    return subtasks.map((subtask: any) => {
-      if (subtask.ParentId === parentId) {
-        subtask.View = isChecked;
-        subtask.Add = isChecked;
-        subtask.Edit = isChecked;
-        subtask.Active = isChecked;
-        subtask.Delete = isChecked;
-        subtask.Export = isChecked;
-      }
-      return subtask;
-    });
-  }
+  // updateSubtaskPermissions(subtasks: any[], featureId: number, parentId: number, isChecked: boolean): any[] {
+  //   return subtasks.map((subtask: any) => {
+  //     if (subtask.ParentId === parentId) {
+  //       subtask.View = isChecked;
+  //       subtask.Add = isChecked;
+  //       subtask.Edit = isChecked;
+  //       subtask.Active = isChecked;
+  //       subtask.Delete = isChecked;
+  //       subtask.Export = isChecked;
+  //     }
+  //     return subtask;
+  //   });
+  // }
 
 
   onActionComplete(args: ActionEventArgs): void {
@@ -141,6 +141,58 @@ export class PermisiionTreeClientComponent implements OnInit {
 
 
 
+  checkboxChange(args: any): void {
+    const isChecked = args.checked;
+    const featureId = args.rowData.FeatureId;
+
+    this.data = this.data || [];
+    let updatedData = this.updateFeaturePermissions(this.data, featureId, isChecked);
+
+    if (updatedData) {
+      this.data = updatedData;
+    }
+  }
+
+  updateFeaturePermissions(data: any[], featureId: number, isChecked: boolean): any[] | null {
+    return data.map((item: any) => {
+      if (item.FeatureId === featureId) {
+        // Update the parent item
+        item.View = isChecked;
+        item.Add = isChecked;
+        item.Edit = isChecked;
+        item.Active = isChecked;
+        item.Delete = isChecked;
+        item.Export = isChecked;
+
+        // Recursively update child items
+        if (item.subtasks) {
+          item.subtasks = this.updateSubtaskPermissions(item.subtasks, isChecked);
+        }
+      } else if (item.subtasks) {
+        // If the item has subtasks, recursively update them
+        item.subtasks = this.updateSubtaskPermissions(item.subtasks, isChecked);
+      }
+      return item;
+    });
+  }
+
+  updateSubtaskPermissions(subtasks: any[], isChecked: boolean): any[] {
+    return subtasks.map((subtask: any) => {
+      // Update each subtask with the same state as the parent
+      subtask.View = isChecked;
+      subtask.Add = isChecked;
+      subtask.Edit = isChecked;
+      subtask.Active = isChecked;
+      subtask.Delete = isChecked;
+      subtask.Export = isChecked;
+
+      // Recursively update nested subtasks if available
+      if (subtask.subtasks) {
+        subtask.subtasks = this.updateSubtaskPermissions(subtask.subtasks, isChecked);
+      }
+      return subtask;
+    });
+  }
 
 
 
